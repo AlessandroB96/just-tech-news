8e2b365d139434e1b1cd8cbfdda6e98624bbf5ef
AlessandroB96 <abelaj7196@gmail.com>
2022-03-18 13:20:20 -0400

    added login/logout

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
 
 //if we make an endpoitn request that doesnt exist, 404 err
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(session(sess));
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//when you have front-end files like images, stylesheets, and js files, its good to have express.js middleware function that can take all of these files and seerves them statically
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on routes
 app.use(routes);
@@ -17,3 +40,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
+
